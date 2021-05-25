5c2994a0852dd79d3b529bc36b4529ccd28adb5a
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-05-25 19:57:29 -0400

    adds design details

--- js/script.js
+++ js/script.js
@@ -33,8 +33,8 @@ function getData(endpoint) {
 function renderTrainers() {
   $("#trainers").html("");
   let html = apiData.map((item) => {
-    return `<article class="card m-2" style="width: 18rem;"><img src=${item.avatar} class="card-img-top"><div class="card-body">
-    <h3 class="card-title">${item.name}</h3><p class="card-text">${item.info}</p><a href="#" class="card-link">More Details</a></div></article>`;
+    return `<article class="card m-2" ><img src=${item.avatar} class="card-img-top"><div class="card-body">
+    <h3 class="card-title">${item.name}</h3><p class="card-text">${item.info}</p><a href="#" class="card-link">More Info</a></div></article>`;
   });
   if (!showMore) {
     $("#trainers").append(html.splice(0, 3));
@@ -47,8 +47,8 @@ function renderTrainers() {
 
 function renderClasses() {
   let html = apiData.map((item) => {
-    return `<article class="card m-1 mw-75"><img src=${item.image} class="card-img-top"><div class="card-body">
-    <h3 class="card-title">${item.title}</h3><p class="card-text">${item.type}</p></div></article>`;
+    return `<div class="class  d-flex flex-column justify-content-between p-2 mt-2" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${item.image})"> 
+    <h3 class="">${item.title}</h3><p class="">${item.type}</p><button class="btn w-50 " style="margin-left: auto">Sign Up</button></div>`;
   });
   $("#classes").append(html);
 }
